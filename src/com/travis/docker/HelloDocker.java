06b5e2eb27fe8273183dd1fc654e0f99603af516
Les <Les@Les-PC>
2017-12-06 18:05:59 -0300

    trw3ewrtwe

---
+++ src/com/travis/docker/HelloDocker.java
@@ -0,0 +1,11 @@
+package com.travis.docker;
+
+public class HelloDocker {
+
+	public static void main(String[] args) {
+
+		System.out.println("==============================\n");
+		System.out.println("Hello docker!!!\n");
+		System.out.println("==============================");
+	}
+}
